bbd54e50019bc45d97e436aef34b3ef9b08255e0
Chase Manning <me@chasemanning.co.nz>
2021-07-03 21:29:37 +1200

    :rocket: add waifus to home page slides

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Slide from "../components/Slide";
+import i01 from "../assets/v2/01.png";
+import i02 from "../assets/v2/02.png";
+import i03 from "../assets/v2/03.png";
+import i04 from "../assets/v2/04.png";
+import i05 from "../assets/v2/05.png";
+import i06 from "../assets/v2/06.png";
+import i07 from "../assets/v2/07.png";
 
 const Container = styled.div`
   width: 100%;
@@ -11,7 +18,13 @@ const Container = styled.div`
 const Home: React.FC = () => {
   return (
     <Container>
-      <Slide color="red" />
+      <Slide color="#9E4E64" image={i05} />
+      <Slide color="#D3AA73" image={i04} right />
+      <Slide color="#A06758" image={i02} />
+      <Slide color="#4C96B2" image={i06} right />
+      <Slide color="#6A8154" image={i03} />
+      <Slide color="#C584A4" image={i01} right />
+      <Slide color="#6E4C65" image={i07} />
     </Container>
   );
 };
