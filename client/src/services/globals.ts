9966e84524088806a17d905f4da2263686111159
Chase Manning <me@chasemanning.co.nz>
2021-07-03 22:49:09 +1200

    :sparkles: add waifu price to globals

--- client/src/services/globals.ts
+++ client/src/services/globals.ts
@@ -9,21 +9,25 @@ export enum Network {
 export type GlobalsData = {
   network: Network;
   waifuAddress: string;
+  price: string;
 };
 
 export const TEST_GLOBALS: GlobalsData = {
   network: Network.TEST,
   waifuAddress: "0xdb5b6d8058a8B3514c603B997Ee1810cD788ddeE",
+  price: "0.008",
 };
 
 export const ETH_GLOBALS: GlobalsData = {
   network: Network.ETH,
   waifuAddress: "0x2216d47494E516d8206B70FCa8585820eD3C4946",
+  price: "0.008",
 };
 
 export const BSC_GLOBALS: GlobalsData = {
   network: Network.BSC,
   waifuAddress: "0x2216d47494E516d8206B70FCa8585820eD3C4946",
+  price: "0.008",
 };
 
 export const getGlobals = async (): Promise<GlobalsData> => {
