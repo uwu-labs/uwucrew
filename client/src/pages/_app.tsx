3d9ce08a44c4dc0f181fc78760c16b6d4582148c
QuantumlyTangled <ethoarx@gmail.com>
2021-07-05 22:05:06 +0200

    feat: basic seo properties

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { SITE } from 'core/constants';
+import { DefaultSEOProps } from 'DefaultSEOProps';
 import type { NextPage } from 'next';
 import PlausibleProvider from 'next-plausible';
+import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
 			<PlausibleProvider domain={SITE}>
+				<Head>
+					<meta httpEquiv="Expires" content="1y" />
+					<meta httpEquiv="Pragma" content="1y" />
+					<meta httpEquiv="Cache-Control" content="1y" />
+				</Head>
+				<DefaultSeo {...DefaultSEOProps} />
+
 				<Component {...pageProps} />
 			</PlausibleProvider>
 		</>
