afb5b7aa0e0f4eaea17a406c76850a039c9e3a03
Nejc Drobnic <ethoarx@gmail.com>
2021-07-05 11:06:29 +0200

    feat: basic nextjs

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const IndexPage: NextPage = () => {
+	return (
+		<>
+			<h1>Hi</h1>
+		</>
+	);
+};
+
+export default IndexPage;
