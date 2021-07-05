dd58e59ef7b81323b621a55b8d232d2d5ba70467
QuantumlyTangled <ethoarx@gmail.com>
2021-07-05 22:06:07 +0200

    fix: conflict between fontawesome and next-seo

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,6 +7,9 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 
+import { config, dom } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
@@ -15,6 +18,9 @@ const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 					<meta httpEquiv="Expires" content="1y" />
 					<meta httpEquiv="Pragma" content="1y" />
 					<meta httpEquiv="Cache-Control" content="1y" />
+
+					{/* This fixes big icons with next-seo since CSS is somehow overwritten*/}
+					<style>{dom.css()}</style>
 				</Head>
 				<DefaultSeo {...DefaultSEOProps} />
 
