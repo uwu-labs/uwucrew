f04dee7c3ff6e37c553f79e283b927fe09fecd26
Chase Manning <me@chasemanning.co.nz>
2021-07-10 12:01:06 +1200

    :lipstick: add global styling

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -11,10 +11,11 @@ import girl04 from '../assets/girls/04.png';
 import girl06 from '../assets/girls/06.png';
 import girl07 from '../assets/girls/07.png';
 import BuySlide from '../components/BuySlide';
+import GlobalStyles from 'styles/GlobalStyles';
 
 const Container = styled.div`
 	position: relative;
-	width: 100%;
+	width: 100vw;
 	display: flex;
 	flex-direction: column;
 	height: 100%;
@@ -22,72 +23,75 @@ const Container = styled.div`
 
 const IndexPage: NextPage = () => {
 	return (
-		<Container>
-			<Slide
-				color="#4C96B2"
-				image={girl06}
-				section=""
-				header="Project Name"
-				subHeaders={[
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-				]}
-			/>
-			<BuySlide />
-			<Slide
-				color="#D3AA73"
-				image={girl04}
-				section="about"
-				header="About Project Name"
-				subHeaders={[
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt '
-				]}
-			/>
-			<Slide
-				color="#6E4C65"
-				image={girl07}
-				right
-				section="roadmap"
-				header="Project Name Roadmap"
-				subHeaders={[
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-				]}
-			/>
-			<Slide
-				color="#A06758"
-				image={girl02}
-				section="team"
-				header="The Project Name Team"
-				subHeaders={[
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-				]}
-			/>
-			<Slide
-				color="#6A8154"
-				image={girl03}
-				right
-				section="provenance"
-				header="NFT Name Provenance"
-				subHeaders={[
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-				]}
-			/>
-			<Slide
-				color="#C584A4"
-				image={girl01}
-				section="faq"
-				header="Project Name FAQs"
-				subHeaders={[
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-				]}
-			/>
-		</Container>
+		<>
+			<GlobalStyles />
+			<Container>
+				<Slide
+					color="#4C96B2"
+					image={girl06}
+					section=""
+					header="Project Name"
+					subHeaders={[
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+					]}
+				/>
+				<BuySlide />
+				<Slide
+					color="#D3AA73"
+					image={girl04}
+					section="about"
+					header="About Project Name"
+					subHeaders={[
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt '
+					]}
+				/>
+				<Slide
+					color="#6E4C65"
+					image={girl07}
+					right
+					section="roadmap"
+					header="Project Name Roadmap"
+					subHeaders={[
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+					]}
+				/>
+				<Slide
+					color="#A06758"
+					image={girl02}
+					section="team"
+					header="The Project Name Team"
+					subHeaders={[
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+					]}
+				/>
+				<Slide
+					color="#6A8154"
+					image={girl03}
+					right
+					section="provenance"
+					header="NFT Name Provenance"
+					subHeaders={[
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+					]}
+				/>
+				<Slide
+					color="#C584A4"
+					image={girl01}
+					section="faq"
+					header="Project Name FAQs"
+					subHeaders={[
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+					]}
+				/>
+			</Container>
+		</>
 	);
 };
 
