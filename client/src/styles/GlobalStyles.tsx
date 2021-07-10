02d6503f88901090d8e53af19119d4d6696b0f91
Chase Manning <me@chasemanning.co.nz>
2021-07-10 12:08:22 +1200

    :art: improve formatting

--- client/src/styles/GlobalStyles.tsx
+++ client/src/styles/GlobalStyles.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     :root {
-	    --text-primary: #222222;
+	--text-primary: #222222;
     }
 
     * {
