79cb83f48fa5d321d12c6df9eec6d6ae2d208223
Chase Manning <me@chasemanning.co.nz>
2021-07-19 16:07:16 +1200

    :wrench: change placeholder price to 0.008 eth

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -2,11 +2,11 @@ export const SITE = 'waifusion.io';
 export const SITE_PROTOCOL = `https://${SITE}`;
 export const LIVE = true;
 export const INFURA_ID = '48cba79f8c2b4d80af39d9983bf188a2';
-export const PRICE = '0.04';
+export const PRICE = 0.008;
 
 export const DESCRIPTION: string = [
 	'Waifusion is a digital Waifu collection.',
 	'There are 16,384 guaranteed-unique Waifusion NFTs. They’re just like you; a beautiful work of art, but 2-D and therefore, superior, Anon-kun.',
 	'Each Waifu is wholly unique and yours forever... unless you sell them... Baka.'
 ].join(' ');
-export const contract: string = '0x0fEa23896fa67e370A48B8919fAb60Abff721Cd8';
+export const CONTRACT: string = '0x0fEa23896fa67e370A48B8919fAb60Abff721Cd8';
