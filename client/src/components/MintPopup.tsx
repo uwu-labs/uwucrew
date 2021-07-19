a51ab85a889d1cf9c9ac2f176f7cab807c3a9b9e
Chase Manning <me@chasemanning.co.nz>
2021-07-19 16:07:01 +1200

    :bug: fix issues with mint popup not minting

--- client/src/components/MintPopup.tsx
+++ client/src/components/MintPopup.tsx
@@ -1,36 +1,11 @@
-import { useWeb3React } from '@web3-react/core';
-import { LIVE } from 'core/constants';
-import { MINUTES } from 'countdown';
-import type { BooleanArraySupportOption } from 'prettier';
+import { CONTRACT, PRICE } from 'core/constants';
+import { Contract, ethers } from 'ethers';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from './Button';
-import ConnectWallet from './ConnectWallet';
 import Popup from './Popup';
-
-const StyledHero = styled.div`
-	position: relative;
-	height: 90vh;
-	width: 100%;
-	overflow: hidden;
-`;
-
-const Image = styled.img`
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	opacity: 0.7;
-`;
-
-const ContentContainer = styled.div`
-	position: relative;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
+import abi from '../contracts/WaifusionV2.json';
+import { useWeb3React } from '@web3-react/core';
 
 const Content = styled.div`
 	display: flex;
@@ -38,42 +13,19 @@ const Content = styled.div`
 	align-items: center;
 `;
 
-const Header = styled.h1`
-	font-size: 7rem;
-	letter-spacing: 0.3rem;
-	color: var(--text-primary);
-`;
-
-const SubHeader = styled.h2`
-	font-size: 1.4rem;
-	font-weight: 500;
-	color: var(--text-primary);
-	max-width: 60rem;
-	text-align: center;
-	margin-top: 1rem;
-	line-height: 2rem;
-	margin-bottom: 3rem;
-`;
-
-const Countdown = styled.div`
-	font-size: 3.5rem;
-	font-weight: 500;
-	color: var(--text-primary);
-	margin-bottom: 5rem;
-`;
-
 interface Props {
 	show: boolean;
 	close: () => void;
 }
 
-const Hero = ({ show, close }: Props) => {
+const MintPopup = ({ show, close }: Props) => {
 	const [amount, setAmmount] = useState(0);
+	const { library } = useWeb3React();
 
 	const mint = async () => {
-		const girls = new Contract(AD, girlsAbi, library.getSigner());
-		await girls.mint(Number(amount), {
-			value: ethers.utils.parseEther((0.008 * Number(amount)).toString())
+		const contract = new Contract(CONTRACT, abi, library.getSigner());
+		await contract.mint(Number(amount), {
+			value: ethers.utils.parseEther((PRICE * Number(amount)).toString())
 		});
 	};
 
@@ -92,4 +44,4 @@ const Hero = ({ show, close }: Props) => {
 	);
 };
 
-export default Hero;
+export default MintPopup;
