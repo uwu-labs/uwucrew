3c769ace54de76a84cfaf66e3004e3cecc74fcd7
Chase Manning <me@chasemanning.co.nz>
2021-08-08 12:15:58 +1200

    :bug: footer cropped

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -74,8 +74,8 @@ const IndexPage: NextPage = () => {
 						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 					]}
 				/> */}
+				<Footer />
 			</Container>
-			<Footer />
 		</>
 	);
 };
