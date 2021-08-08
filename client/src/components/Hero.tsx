652491ea46ae9a70470e377f51b4e297dc76d7c8
Chase Manning <me@chasemanning.co.nz>
2021-08-08 12:39:59 +1200

    :art: change hero to use rotating image

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,19 +1,12 @@
 import { useWeb3React } from '@web3-react/core';
 import { LIVE } from 'core/constants';
 import React, { useEffect, useRef, useState } from 'react';
-import Image from 'next/image';
 import styled, { keyframes } from 'styled-components';
 import Button from './Button';
 import ConnectWallet from './ConnectWallet';
 import MintPopup from './MintPopup';
-import uwu01 from '../assets/girls/01.png';
-import uwu02 from '../assets/girls/02.png';
-import uwu03 from '../assets/girls/03.png';
-import uwu04 from '../assets/girls/04.png';
-import uwu05 from '../assets/girls/05.png';
-import uwu06 from '../assets/girls/06.png';
-import uwu07 from '../assets/girls/07.png';
 import EmailSignup from './EmailSignup';
+import RotatingImage from './RotatingImage';
 
 const colors: string[] = ['var(--bg-01)', 'var(--bg-02)', 'var(--bg-03)', 'var(--bg-04)', 'var(--bg-05)'];
 
@@ -28,60 +21,11 @@ const raise = keyframes`
   }
 `;
 
-const slide = keyframes`
-  from {
-	  opacity: 0;
-	transform: translateX(-100%);
-  }
-  to {
-	  opacity: 1;
-	transform: translateX(0);
-  }
-`;
-
 const StyledHero = styled.div``;
 
-interface ImageProps {
+interface ContentContainerProps {
 	color: string;
 }
-
-const StyledImage = styled.div`
-	position: relative;
-	width: 35vw;
-	height: 32vw;
-	padding: 2rem;
-	padding-bottom: 0;
-	background-color: ${(props: ImageProps) => props.color};
-	transition: all 1s;
-	overflow: hidden;
-
-	opacity: 0;
-	transform: translateX(100%);
-	animation: ${slide} 1s 3.2s ease-out forwards;
-
-	@media (max-width: 768px) {
-		width: 100%;
-		height: 100%;
-		padding: 0;
-	}
-`;
-
-interface TransformProps {
-	transform: string;
-}
-
-const ImageTransform = styled.div`
-	position: absolute;
-	bottom: 0;
-	left: 0;
-	width: 100%;
-	transform: translateX(${(props: TransformProps) => props.transform});
-	transition: all 1s;
-
-	> div {
-		transform: translate(-2%, 4px);
-	}
-`;
 
 const ContentContainer = styled.div`
 	overflow: hidden;
@@ -89,7 +33,7 @@ const ContentContainer = styled.div`
 	min-height: 100vh;
 	width: 100%;
 	overflow: hidden;
-	background-color: ${(props: ImageProps) => props.color};
+	background-color: ${(props: ContentContainerProps) => props.color};
 	background-image: radial-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.75));
 	display: flex;
 	align-items: center;
@@ -204,10 +148,6 @@ const Hero = () => {
 	const colorRef = useRef(color);
 	colorRef.current = color;
 
-	const imageTransform = (position: number): string => {
-		return (position - color) * 100 + '%';
-	};
-
 	useEffect(() => {
 		setInterval(() => {
 			if (colorRef.current < 4) setColor(colorRef.current + 1);
@@ -219,29 +159,7 @@ const Hero = () => {
 		<StyledHero>
 			<ContentContainer color={colors[color]}>
 				<ImageContainer>
-					<StyledImage color={colors[color]}>
-						<ImageTransform transform={imageTransform(0)}>
-							<Image src={uwu01} />
-						</ImageTransform>
-						<ImageTransform transform={imageTransform(1)}>
-							<Image src={uwu02} />
-						</ImageTransform>
-						<ImageTransform transform={imageTransform(2)}>
-							<Image src={uwu03} />
-						</ImageTransform>
-						<ImageTransform transform={imageTransform(3)}>
-							<Image src={uwu04} />
-						</ImageTransform>
-						<ImageTransform transform={imageTransform(4)}>
-							<Image src={uwu05} />
-						</ImageTransform>
-						<ImageTransform transform={imageTransform(5)}>
-							<Image src={uwu06} />
-						</ImageTransform>
-						<ImageTransform transform={imageTransform(6)}>
-							<Image src={uwu07} />
-						</ImageTransform>
-					</StyledImage>
+					<RotatingImage color={colors[color]} activeIndex={color} />
 				</ImageContainer>
 				<TextContainer>
 					<ComingSoonContainer>
