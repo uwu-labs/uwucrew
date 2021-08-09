946d442a45d75219aeb87198d08f3dfc2e754edb
Chase Manning <me@chasemanning.co.nz>
2021-08-09 21:40:43 +1200

    :lipstick: update button styling

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,17 +1,27 @@
 import styled from 'styled-components';
 
-const Button = styled.button<{
-	primary?: boolean;
-}>`
-	color: var(--bg);
-	font-size: 2rem;
-	font-weight: 600;
-	position: relative;
-	cursor: pointer;
-	padding: 0.5rem 1rem;
-	background: var(--text-primary);
-	border: solid 2px var(--text-primary);
+interface Props {
+	color: string;
+}
+
+const Button = styled.button`
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0 2rem;
+	background-color: ${(props: Props) => props.color};
+	transition: all 1s;
+	color: white;
+	font-size: 1.8rem;
+	font-weight: 500;
 	text-transform: uppercase;
+	cursor: pointer;
+	padding: 0 5rem;
+
+	@media (max-width: 768px) {
+		padding: 0 1rem;
+	}
 `;
 
 export default Button;
