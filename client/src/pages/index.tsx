a33f3b7c2ba8451955cd948ede4841460a76d52e
Chase Manning <me@chasemanning.co.nz>
2021-08-10 22:05:17 +1200

    :art: update landing page to use new components

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,8 @@ import GlobalStyles from 'styles/GlobalStyles';
 import Header from '../components/Header';
 import Hero from 'components/Hero';
 import Slide from 'components/Slide';
+import AboutSlide from 'components/AboutSlide';
+import LoreSlide from 'components/LoreSlide';
 
 const Container = styled.div`
 	position: relative;
@@ -22,16 +24,8 @@ const IndexPage: NextPage = () => {
 			<Header />
 			<Container>
 				<Hero />
-				<Slide
-					color="var(--bg-01)"
-					section="about"
-					header="About"
-					subHeaders={[
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt '
-					]}
-				/>
+				<AboutSlide />
+				<LoreSlide />
 				<Slide
 					color="var(--bg-02)"
 					section="roadmap"
@@ -49,15 +43,6 @@ const IndexPage: NextPage = () => {
 						'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
 						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ',
 						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-					]}
-				/>
-				<Slide
-					color="var(--bg-04)"
-					section="lore"
-					header="The Lore"
-					subHeaders={[
-						'Do you fight with your siblings? Don’t we all? Our story about sisters Kaitlyn and Ella is no different except for one thing, these two were powerful beings made out of pure cosmic energy. Kait and Ella were absolutely the same, but completely different. Kait was wise but cunning, she was made out of light energy called uwu, while Ella was cunning but wise, she got formed from owo, dark cosmic energy.',
-						'Usually, their fights were lighthearted. For example, Kait liked to go inside Ella’s interdimensional room intentionally leaving the intergalactic door open, while Ella liked exploding stars that Kait liked. All of it was fine, until one day Ella accidentally spilled Kait’s iced coffee made out of energy from the sun and the biggest sibling clash in the history of the universe began.'
 					]}
 				/>
 				<Slide
