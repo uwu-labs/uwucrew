242a29120db1f0db3a4d06ed39ab4e95e68f76f5
Chase Manning <me@chasemanning.co.nz>
2021-08-18 09:02:49 +1200

    :memo: update trait count

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -176,7 +176,7 @@ const Hero = () => {
 					<Header>uwucrew</Header>
 					<SubHeader>
 						uwucrew is a generative collection of 9670 avatars inspired by anime and pop culture, aiming to be both inclusive and
-						expressive. Every uwucrew NFT is completely unique and features up to 8 traits with 120+ assets.
+						expressive. Every uwucrew NFT is completely unique and features up to 9 traits with 120+ assets.
 					</SubHeader>
 					<SubHeader>uwucrew NFTs will cost 0.06 ETH to mint and release late August.</SubHeader>
 					{LIVE && (
