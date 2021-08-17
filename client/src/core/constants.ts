1f5dce296881e4e0f62add2543ac64ba06b8d92c
Chase Manning <me@chasemanning.co.nz>
2021-08-18 08:55:31 +1200

    :memo: remove femme-focused

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -5,5 +5,5 @@ export const INFURA_ID = '48cba79f8c2b4d80af39d9983bf188a2';
 export const PRICE = 0.008;
 
 export const DESCRIPTION: string =
-	'uwucrew is a generative collection of completely unique 9670 femme-focused avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
+	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
 export const CONTRACT: string = '0x0fEa23896fa67e370A48B8919fAb60Abff721Cd8';
