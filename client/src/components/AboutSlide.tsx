d388b18d9d30f3eff300487a38c6ce943e562552
Chase Manning <me@chasemanning.co.nz>
2021-08-17 12:34:55 +1200

    :memo: update about page wording

--- client/src/components/AboutSlide.tsx
+++ client/src/components/AboutSlide.tsx
@@ -8,7 +8,7 @@ const AboutSlide = () => {
 			section="about"
 			header="About"
 			subHeaders={[
-				'uwucrew is a generative collection of 10,000 female-focused avatars inspired by anime and pop culture, aiming to be both inclusive and expressive. Every uwucrew NFT is completely unique and features up to 8 traits - Background, Hat, Hair, Eyes, Mouth, Top, Left Accessory, and Right Accessory.',
+				'uwucrew is a generative collection of 9970 feminine-focused avatars inspired by anime and pop culture, aiming to be both inclusive and expressive. Every uwucrew NFT is completely unique and features up to 8 traits - Background, Hat, Hair, Eyes, Mouth, Top, Left Accessory, and Right Accessory.',
 				'An interesting property of uwucrew is our unique asset generation which involves over 25 layers of hand-drawn art! In order to support various kinds of outfits and expressions, the uwucrew collection also supports multiple arm poses (even with sleeved clothes) to let us incorporate several kinds of hand accessories!'
 			]}
 		/>
