7dfc540ad784f7e9852f8b66eab42a418e9acac5
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-08-25 22:26:28 -0400

    update deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,25 @@ async function main() {
   let primary = signers[0];
 
   console.log("deploying from: ", primary);
-  
+
   let UwUCrew = await ethers.getContractFactory("uwucrew");
   uwucrew = await UwUCrew.connect(primary).deploy("uwucrew", "UWU", 9670);
   await uwucrew.deployed();
 
   let Sales = await ethers.getContractFactory("uwucrewWaveLockSaleWithMint");
   // Total: 9670
-  salesContract = await Sales.connect(primary).deploy(uwucrew.address, primary.address, 10, 8000 /* sale count */, 1370 /* swap count */, 50 /* lpAmount */); 
+  salesContract = await Sales.connect(primary).deploy(uwucrew.address, "0x354A70969F0b4a4C994403051A81C2ca45db3615", 10, 8000 /* sale count */, 1370 /* swap count */, 50 /* lpAmount */); 
   // Purposely leaving out 50 for team, and 200 for BSC.
   await salesContract.deployed();
   await uwucrew.connect(primary).prepareSale(salesContract.address);
 
+  await uwucrew.transferOwnership("0x354A70969F0b4a4C994403051A81C2ca45db3615");
+
   console.log("uwucrew deployed to:", uwucrew.address);
   console.log("sales contract deployed to:", salesContract.address);
+
+  console.log("uwu owner:", uwucrew.address);
+  console.log("sale owner:", salesContract.address);
 }
 
 main()
