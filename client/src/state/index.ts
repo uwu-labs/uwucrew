8527539d88a029c8f0af6bacc1206fddea3c9fb4
Chase Manning <me@chasemanning.co.nz>
2021-08-30 17:08:41 +1200

    :sparkles: create uwu state

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -1,16 +1,17 @@
 import { applyMiddleware, combineReducers, createStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-import userReducer from 'state/reducers/user';
+import uwuReducer from 'state/reducers/uwu';
 import navigationReducer from './reducers/navigation';
+import thunk from 'redux-thunk';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const reducers = combineReducers({
-	user: userReducer,
+	uwu: uwuReducer,
 	navigation: navigationReducer
 });
 
-export const store = createStore(reducers, composeWithDevTools({ trace: true })(applyMiddleware(sagaMiddleware)));
+export const store = createStore(reducers, composeWithDevTools({ trace: true })(applyMiddleware(sagaMiddleware), applyMiddleware(thunk)));
 
 export type RootState = ReturnType<typeof store.getState>;
