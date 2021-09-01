8044d9b65508a9b7f5a9b9105a6f5dc2d4038029
Chase Manning <me@chasemanning.co.nz>
2021-09-01 23:23:43 +1200

    :lipstick: style owned tickets

--- client/src/components/OwnedTickets.tsx
+++ client/src/components/OwnedTickets.tsx
@@ -8,12 +8,16 @@ import { selectOwnedTickets } from 'state/reducers/uwu';
 const StyledOwnedTickets = styled.div`
 	display: flex;
 	align-items: center;
+	border: solid 3px var(--bg-04);
+	padding: 0.1rem 1rem;
 `;
 
 const Label = styled.div`
 	font-weight: 500;
 	color: var(--text-primary);
 	font-family: 'Roboto', sans-serif;
+	margin-right: 1rem;
+	color: var(--bg-04);
 
 	font-size: 2rem;
 	line-height: 2.3rem;
@@ -24,7 +28,7 @@ const Label = styled.div`
 `;
 
 const Icon = styled.div`
-	height: 1.6rem;
+	width: 4rem;
 	margin-top: 0.2rem;
 `;
 
@@ -33,7 +37,7 @@ const OwnedTickets = () => {
 
 	return (
 		<StyledOwnedTickets>
-			<Label>{`Tickets Owned: ${ownedTickets}`}</Label>
+			<Label>{`Owned: ${ownedTickets}`}</Label>
 			<Icon>
 				<Image src={ticket} />
 			</Icon>
