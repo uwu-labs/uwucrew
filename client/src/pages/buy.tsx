e29829bbdd7c2db3094d8e7fd92fb7c27246d05f
Chase Manning <me@chasemanning.co.nz>
2021-09-02 14:04:14 +1200

    :rocket: add stats to buy price

--- client/src/pages/buy.tsx
+++ client/src/pages/buy.tsx
@@ -2,10 +2,15 @@ import type { NextPage } from 'next';
 import React from 'react';
 import styled from 'styled-components';
 import GlobalStyles from 'styles/GlobalStyles';
+import Image from 'next/image';
+
 import Header from '../components/Header';
+import ticket from '../assets/tickets/uwu_coin.gif';
 import OwnedTickets from 'components/OwnedTickets';
 import ForceConnect from 'components/ForceConnect';
 import { LIVE } from 'core/constants';
+import { useSelector } from 'react-redux';
+import { selectBuyPrice, selectIsLocked, selectRemaining, selectStartTime, selectWave } from 'state/reducers/uwu';
 
 const StyledBuy = styled.div`
 	display: flex;
@@ -14,6 +19,13 @@ const StyledBuy = styled.div`
 	min-height: 100vh;
 	background-color: var(--bg-04);
 	background-image: radial-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.75));
+`;
+
+const HeaderContainer = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const Container = styled.div`
@@ -34,18 +46,72 @@ const HeaderText = styled.div`
 	}
 `;
 
+const Content = styled.div`
+	width: 100%;
+	min-height: 70vh;
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+`;
+
+const Center = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const Label = styled.div`
+	font-weight: 500;
+	color: var(--text-primary);
+	line-height: 2.3rem;
+	margin-bottom: 2rem;
+
+	font-size: 2rem;
+	@media (max-width: 768px) {
+		font-size: 1.6rem;
+		max-width: 80vw;
+	}
+`;
+
+const Ticket = styled.div`
+	width: 40rem;
+`;
+
 const BuyPage: NextPage = () => {
+	const buyPrice = useSelector(selectBuyPrice);
+	const wave = useSelector(selectWave);
+	const remaining = useSelector(selectRemaining);
+	const isLocked = useSelector(selectIsLocked);
+	const startTime = useSelector(selectStartTime);
+
 	if (!LIVE) return <></>;
 
 	return (
 		<StyledBuy>
-			{/* <MintPopup show={true} close={() => console.log('emeo')} /> */}
 			<ForceConnect />
 			<GlobalStyles />
 			<Header />
+
 			<Container>
-				<HeaderText>Buy uwu-tickets</HeaderText>
-				<OwnedTickets />
+				<HeaderContainer>
+					<HeaderText>Buy uwu-tickets</HeaderText>
+					<OwnedTickets />
+				</HeaderContainer>
+				<Content>
+					<Ticket>
+						<Image src={ticket} />
+					</Ticket>
+					<Center>
+						<Label>{`Tickets Remaining: ${remaining}`}</Label>
+						<Label>{`Buy Price: ${buyPrice}`}</Label>
+						<Label>{`Wave: ${wave}`}</Label>
+						<Label>{`isLocked: ${isLocked}`}</Label>
+						<Label>{`Start Time: ${startTime}`}</Label>
+					</Center>
+					<Ticket>
+						<Image src={ticket} />
+					</Ticket>
+				</Content>
 			</Container>
 		</StyledBuy>
 	);
