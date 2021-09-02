a005e3c5a8785c5dded6d3a55ba439602f881a14
Chase Manning <me@chasemanning.co.nz>
2021-09-02 16:57:54 +1200

    :wrench: add prod contract and wave limits

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -6,5 +6,12 @@ export const PRICE = 0.008;
 
 export const DESCRIPTION: string =
 	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
+
+// Rinkeby;
 export const SALE_CONTRACT: string = '0x26C13627520a338eA9C3Dde62B6ac8Cf12163997';
+
+// Prod
+// export const SALE_CONTRACT: string = '0x5e75bc35955f9e196e5bb25ddde09424b476a18d';
 export const UWU_CONTRACT: string = '0x1F64F0f5411c73B9323a788C314737E899553a98';
+
+export const waveLimits: number[] = [4, 16, 32, 32];
