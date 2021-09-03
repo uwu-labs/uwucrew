0b3cceb72009de5deab61802501666b20080fbe9
Chase Manning <me@chasemanning.co.nz>
2021-09-03 13:29:39 +1200

    :rocket: add uwus upload script

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,20 @@ import * as cors from "cors";
 const abi = require("../contracts/uwucrew.json");
 const Web3 = require("web3");
 
+// TODO Rename images
+// TODO Load images
+// TODO setup database
+// TODO Setup database schema
+// TODO Add function to upload uwuws
+// TODO Create database upload app
+// TODO Load into database
+// TODO setep return filter
+// TODO Check Start time
+// TODO Test
+// TODO Add Kiwi to firebase
+// TODO Show Kiwi how it works
+// TODO Add vanity URL
+
 const UWU_CONTRACT = "0x1F64F0f5411c73B9323a788C314737E899553a98";
 
 const defaultUwu = () => {
@@ -36,10 +50,10 @@ app.get("/uwu/:id", async (request: any, response: any) => {
     const web3 = new Web3(provider);
     const contract = new web3.eth.Contract(abi, UWU_CONTRACT);
     const supply = await contract.methods.totalSupply().call();
-    if (id > supply) return response.json(defaultUwu());
+    if (id > supply + 10_000) return response.json(defaultUwu());
 
     let moodQuerySnapshot = await db
-      .collection("uwuws")
+      .collection("uwus")
       .where("id", "==", Number.parseInt(id))
       .get();
 
@@ -47,6 +61,24 @@ app.get("/uwu/:id", async (request: any, response: any) => {
       return response.status(404).send("Uwu Not Found");
 
     return response.json(moodQuerySnapshot.docs[0].data());
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send(error);
+  }
+});
+
+app.post("/uwus", async (request: any, response: any) => {
+  try {
+    const { attributes, id, description, image, name } = request.body;
+    const data = {
+      id,
+      image: `https://firebasestorage.googleapis.com/v0/b/uwucrew-3563e/o/${image}.png?alt=media`,
+      attributes,
+      description,
+      name,
+    };
+    await db.collection("uwus").add(data);
+    return response.status(200).send("Uwu Uploaded");
   } catch (error) {
     console.log(error);
     return response.status(500).send(error);
