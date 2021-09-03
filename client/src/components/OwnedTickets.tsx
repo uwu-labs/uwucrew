9060d98164ed75a1adf42eb2dc38ed318a8ee54c
Chase Manning <me@chasemanning.co.nz>
2021-09-03 19:12:06 +1200

    :lipstick: design reword of owned tickets

--- client/src/components/OwnedTickets.tsx
+++ client/src/components/OwnedTickets.tsx
@@ -1,35 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
-import ticket from '../assets/tickets/uwu_coin.png';
 import { useSelector } from 'react-redux';
 import { selectOwnedTickets } from 'state/reducers/uwu';
 
 const StyledOwnedTickets = styled.div`
 	display: flex;
 	align-items: center;
-	border: solid 3px var(--bg-04);
-	padding: 0.1rem 1rem;
+	background-color: var(--bg-04);
+	height: 4.7rem;
+	padding: 0 3.3rem;
 `;
 
 const Label = styled.div`
 	font-weight: 500;
-	color: var(--text-primary);
-	font-family: 'Roboto', sans-serif;
-	margin-right: 1rem;
-	color: var(--bg-04);
+	color: white;
 
-	font-size: 2rem;
+	font-size: 2.2rem;
 	line-height: 2.3rem;
 	@media (max-width: 768px) {
 		font-size: 1.6rem;
 		line-height: 2rem;
 	}
-`;
-
-const Icon = styled.div`
-	width: 4rem;
-	margin-top: 0.2rem;
 `;
 
 const OwnedTickets = () => {
@@ -38,9 +29,6 @@ const OwnedTickets = () => {
 	return (
 		<StyledOwnedTickets>
 			<Label>{`Owned: ${ownedTickets}`}</Label>
-			<Icon>
-				<Image src={ticket} />
-			</Icon>
 		</StyledOwnedTickets>
 	);
 };
