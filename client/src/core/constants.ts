f48069b8e9258f4f404e8ef3442bf8acebd00688
Chase Manning <me@chasemanning.co.nz>
2021-09-03 19:34:31 +1200

    :wrench: create seconds per block constant

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -2,6 +2,7 @@ export const SITE = 'waifusion.io';
 export const SITE_PROTOCOL = `https://${SITE}`;
 export const INFURA_ID = '48cba79f8c2b4d80af39d9983bf188a2';
 export const PRICE = 0.008;
+export const SECONDS_PER_BLOCK = 13.32;
 
 export const DESCRIPTION: string =
 	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
