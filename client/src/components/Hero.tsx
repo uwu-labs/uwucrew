55f2a881228a9bdfde03f14ed99e15ef9a58f76e
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-09-04 11:12:19 -0400

    Update time

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -146,7 +146,7 @@ const Hero = () => {
 						uwucrew is a generative collection of 9670 avatars inspired by anime and pop culture, aiming to be both inclusive and
 						expressive. Every uwucrew NFT is completely unique and features up to 9 traits with 120+ assets.
 					</SubHeader>
-					<SubHeader>uwucrew NFTs will cost 0.06 ETH to mint and are releasing on Sunday 9/5 at 6pm EST / 3pm PST</SubHeader>
+					<SubHeader>uwucrew NFTs will cost 0.06 ETH to mint and are releasing on Sunday 9/5 at 5:30PM EST / 2:30PM PST</SubHeader>
 					{!LIVE && <Countdown />}
 					<ButtonContainer>
 						<Button
