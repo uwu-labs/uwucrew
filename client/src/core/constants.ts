43627839efb61bcf78f9983f41a5d0c999e718eb
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-09-04 11:16:37 -0400

    Update metadata

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -5,6 +5,6 @@ export const INFURA_ID = '48cba79f8c2b4d80af39d9983bf188a2';
 export const PRICE = 0.008;
 
 export const DESCRIPTION: string =
-	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
+	'uwucrew is a generative NFT collection of 9,670 completely unique avatars inspired by anime & pop culture, aiming to be inclusive & expressive.';
 export const SALE_CONTRACT: string = '0x26C13627520a338eA9C3Dde62B6ac8Cf12163997';
 export const UWU_CONTRACT: string = '0x1F64F0f5411c73B9323a788C314737E899553a98';
