75053f1d09254f5314af45adb36a1991d09d4e9b
Chase Manning <me@chasemanning.co.nz>
2021-09-04 16:14:32 +1200

    :wrench: update addresses

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -2,16 +2,17 @@ export const SITE = 'waifusion.io';
 export const SITE_PROTOCOL = `https://${SITE}`;
 export const INFURA_ID = '48cba79f8c2b4d80af39d9983bf188a2';
 export const PRICE = 0.008;
-export const SECONDS_PER_BLOCK = 13.32;
+// export const SECONDS_PER_BLOCK = 13.32;
+export const SECONDS_PER_BLOCK = 15.2;
 
 export const DESCRIPTION: string =
 	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
 
 // Rinkeby;
-export const SALE_CONTRACT: string = '0x26C13627520a338eA9C3Dde62B6ac8Cf12163997';
+export const SALE_CONTRACT: string = '0x5E75Bc35955F9E196e5bb25ddDE09424B476a18D';
 
 // Prod
 // export const SALE_CONTRACT: string = '0x5e75bc35955f9e196e5bb25ddde09424b476a18d';
-export const UWU_CONTRACT: string = '0x1F64F0f5411c73B9323a788C314737E899553a98';
+export const UWU_CONTRACT: string = '0xF75140376D246D8B1E5B8a48E3f00772468b3c0c';
 
 export const waveLimits: number[] = [4, 16, 32, 32];
