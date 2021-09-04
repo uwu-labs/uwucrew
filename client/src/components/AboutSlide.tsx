2fb5510e401074e9616544f1576839474e1c3f6f
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-09-04 11:11:28 -0400

    Change time

--- client/src/components/AboutSlide.tsx
+++ client/src/components/AboutSlide.tsx
@@ -10,7 +10,7 @@ const AboutSlide = () => {
 			subHeaders={[
 				'uwucrew is a generative collection of 9670 avatars inspired by anime and pop culture, aiming to be both inclusive and expressive. Every uwucrew NFT is completely unique and features up to 9 traits with 120+ assets.',
 				'An interesting property of uwucrew is our unique asset generation which involves over 25 layers of hand-drawn art! In order to support various kinds of outfits and expressions, the uwucrew collection also supports multiple arm poses (even with sleeved clothes) to let us incorporate several kinds of hand accessories!',
-				'uwucrew is set to launch late August at the cost of 0.06 ETH each, featuring a re-imagined minting process which aims to prevent gas wars and make minting a painless and accessible experience. More details on this later!'
+				'uwucrew is set to launch September 5th @ 5:30 PM EST at the cost of 0.06 ETH each, featuring a re-imagined minting process which aims to prevent gas wars and make minting a painless and accessible experience. More details on this later!'
 			]}
 		/>
 	);
