ea9b13b5b1ff15f13e9ea89ffe0eff5d93344272
Chase Manning <me@chasemanning.co.nz>
2021-09-05 16:15:00 +1200

    :wrench: update contracts

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -2,18 +2,18 @@ export const SITE = 'waifusion.io';
 export const SITE_PROTOCOL = `https://${SITE}`;
 export const INFURA_ID = '1704ebb4fcf041619a8b00b00d8d9aa3';
 export const PRICE = 0.008;
-// export const SECONDS_PER_BLOCK = 13.32;
-export const SECONDS_PER_BLOCK = 15.2;
 
 export const DESCRIPTION: string =
 	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
 
 // Rinkeby;
-export const SALE_CONTRACT: string = '0x3D5042670657Bf91e8a37cea3A001714FD8323Ac';
-export const UWU_CONTRACT: string = '0x28021452b7Ef46e6800C87d057fA64492B2505eB';
+export const SALE_CONTRACT: string = '0xFd7387827fc74E0ce1Ca413B3BF87Fb93Fd15cA9';
+export const UWU_CONTRACT: string = '0xcfb5Db1A49524a1F7Ca7f51aFA1d4B04f32C9CA7';
+export const SECONDS_PER_BLOCK = 16;
 
 // Prod
-// export const SALE_CONTRACT: string = '0x5e75bc35955f9e196e5bb25ddde09424b476a18d';
-// export const UWU_CONTRACT: string = '0x28021452b7Ef46e6800C87d057fA64492B2505eB';
+// export const SALE_CONTRACT: string = '0x5E75Bc35955F9E196e5bb25ddDE09424B476a18D';
+// export const UWU_CONTRACT: string = '0xF75140376D246D8B1E5B8a48E3f00772468b3c0c';
+// export const SECONDS_PER_BLOCK = 13.32;
 
 export const waveLimits: number[] = [4, 16, 32, 32];
