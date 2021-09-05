f1577b42809e546f83f762a5f7eca6ca1e395836
Chase Manning <me@chasemanning.co.nz>
2021-09-05 14:16:00 +1200

    :rocket: finish api

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -1,6 +1,6 @@
 export const SITE = 'waifusion.io';
 export const SITE_PROTOCOL = `https://${SITE}`;
-export const INFURA_ID = '48cba79f8c2b4d80af39d9983bf188a2';
+export const INFURA_ID = '1704ebb4fcf041619a8b00b00d8d9aa3';
 export const PRICE = 0.008;
 // export const SECONDS_PER_BLOCK = 13.32;
 export const SECONDS_PER_BLOCK = 15.2;
@@ -10,9 +10,10 @@ export const DESCRIPTION: string =
 
 // Rinkeby;
 export const SALE_CONTRACT: string = '0x3D5042670657Bf91e8a37cea3A001714FD8323Ac';
+export const UWU_CONTRACT: string = '0x28021452b7Ef46e6800C87d057fA64492B2505eB';
 
 // Prod
 // export const SALE_CONTRACT: string = '0x5e75bc35955f9e196e5bb25ddde09424b476a18d';
-export const UWU_CONTRACT: string = '0xF75140376D246D8B1E5B8a48E3f00772468b3c0c';
+// export const UWU_CONTRACT: string = '0x28021452b7Ef46e6800C87d057fA64492B2505eB';
 
 export const waveLimits: number[] = [4, 16, 32, 32];
