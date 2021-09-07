de4f8cd959b1b0f135c9bc50b9705e6daa5ef886
Chase Manning <me@chasemanning.co.nz>
2021-09-07 15:50:58 +1200

    :rocket: add bsc claim address

--- client/src/core/constants.ts
+++ client/src/core/constants.ts
@@ -7,13 +7,15 @@ export const DESCRIPTION: string =
 	'uwucrew is a generative collection of completely unique 9670 avatars inspired by anime & pop culture, aiming to be inclusive & expressive';
 
 // Rinkeby;
-// export const SALE_CONTRACT: string = '0xA17e72b5172E12e9e3A37Ff0DE201aAF3fBA03b4';
-// export const UWU_CONTRACT: string = '0x64d7F507f3635ea5DFdD9FDec1f2fa3CbF66b7fb';
-// export const SECONDS_PER_BLOCK = 16;
+export const SALE_CONTRACT: string = '0xA17e72b5172E12e9e3A37Ff0DE201aAF3fBA03b4';
+export const BSC_CLAIM: string = '0x25cbC1626A5b775546A206ea2fB835883dD06c5a';
+export const UWU_CONTRACT: string = '0x64d7F507f3635ea5DFdD9FDec1f2fa3CbF66b7fb';
+export const SECONDS_PER_BLOCK = 16;
 
 // Prod
-export const SALE_CONTRACT: string = '0x5E75Bc35955F9E196e5bb25ddDE09424B476a18D';
-export const UWU_CONTRACT: string = '0xF75140376D246D8B1E5B8a48E3f00772468b3c0c';
-export const SECONDS_PER_BLOCK = 13.4;
+// export const SALE_CONTRACT: string = '0x5E75Bc35955F9E196e5bb25ddDE09424B476a18D';
+// export const BSC_CLAIM: string = '0x25cbC1626A5b775546A206ea2fB835883dD06c5a';
+// export const UWU_CONTRACT: string = '0xF75140376D246D8B1E5B8a48E3f00772468b3c0c';
+// export const SECONDS_PER_BLOCK = 13.4;
 
 export const waveLimits: number[] = [4, 16, 32, 32];
