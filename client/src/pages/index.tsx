6a7bc2ff0947c903fcad113cf8ac0fbc5b728eb8
Chase Manning <me@chasemanning.co.nz>
2021-09-15 19:40:40 +1200

    :rotating_light: lint warnings

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 import GlobalStyles from 'styles/GlobalStyles';
 import Header from '../components/Header';
 import Hero from 'components/Hero';
-import Slide from 'components/Slide';
 import AboutSlide from 'components/AboutSlide';
 import TeamSlide from 'components/TeamSlide';
 import RoadmapSlide from 'components/RoadmapSlide';
@@ -28,25 +27,6 @@ const IndexPage: NextPage = () => {
 				<AboutSlide />
 				<RoadmapSlide />
 				<TeamSlide />
-				{/* <LoreSlide /> */}
-				{/* <Slide
-					color="var(--bg-02)"
-					section="roadmap"
-					header="What's Next"
-					subHeaders={[
-						'We are a team who have been greatly involved in the NFT scene since early March, and are extremely excited to create a collection of our own for everyone to enjoy!',
-						'We have several initiatives planned involving a merch shop, NFT drops, and collaborations to reward holders over time. We believe in a long term vision and are committed to our community to provide an innovative and fun experience.',
-						'More announcements will be released in the coming weeks with further details!'
-					]}
-				/> */}
-				{/* <Slide
-					color="var(--bg-05)"
-					section="provenance"
-					header="Provenance"
-					subHeaders={[
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-					]}
-				/> */}
 			</Container>
 		</>
 	);
