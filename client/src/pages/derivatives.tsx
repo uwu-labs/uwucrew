9d47546b28660fda6301c3e825272167751b1dab
Chase Manning <me@chasemanning.co.nz>
2021-12-05 13:57:04 +0000

    :rocket: add derivatives page

---
+++ client/src/pages/derivatives.tsx
@@ -0,0 +1,37 @@
+import { generateSitemap } from 'core/sitemap';
+import type { GetStaticProps, NextPage } from 'next';
+import path from 'path';
+import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from 'styles/GlobalStyles';
+import Header from '../components/Header';
+import Derivatives from 'components/Derivatives';
+
+const Container = styled.div`
+	position: relative;
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+`;
+
+const DerivativesPage: NextPage = () => {
+	return (
+		<>
+			<GlobalStyles />
+			<Header />
+			<Container>
+				<Derivatives />
+			</Container>
+		</>
+	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const directory = path.join(process.cwd(), 'src');
+
+	await generateSitemap(directory);
+
+	return { props: {} };
+};
+
+export default DerivativesPage;
