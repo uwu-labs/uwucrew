b0e6489f4b86fb833eaf382cea51aeefcaebdb28
chase-manning <chase@manning.dev>
2022-04-09 19:44:59 +0100

    add license page

---
+++ client/src/components/License.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledLicense = styled.div`
+	display: flex;
+`;
+
+const License = () => {
+	return <StyledLicense>meow</StyledLicense>;
+};
+
+export default License;
