02432803c05019cff9efd63a2e03300bfb618ee7
chase-manning <chase@manning.dev>
2022-04-12 21:20:37 +0100

    minor gramatical change

--- client/src/components/License.tsx
+++ client/src/components/License.tsx
@@ -151,7 +151,7 @@ const License = () => {
 	return (
 		<StyledLicense>
 			<Content>
-				<Header>uwu art license</Header>
+				<Header>uwu Art License</Header>
 				<SmallHeader>Holders</SmallHeader>
 				<Link href="http://creativecommons.org/licenses/by/4.0" target="_blank">
 					CC BY 4.0
