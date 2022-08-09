5ef192e73a8891b5c7ab6297600ba28c5de3757d
Ivan Martinez <ivanthegreatdev@gmail.com>
2022-08-09 12:01:12 -0400

    Fix metadata more

--- client/src/DefaultSEOProps.ts
+++ client/src/DefaultSEOProps.ts
@@ -2,8 +2,8 @@ import { DESCRIPTION, SITE_PROTOCOL } from 'core/constants';
 import type { DefaultSeoProps as DefaultSeoPropsType } from 'next-seo';
 
 export const DefaultSEOProps: DefaultSeoPropsType = {
-	titleTemplate: 'uwucrew NFTs by uwu Labs| %s',
-	title: 'uwucrew by uwu Labs',
+	titleTemplate: 'uwucrew anime NFTs by uwu Labs',
+	title: 'uwucrew anime NFTs by uwu Labs',
 	description: DESCRIPTION,
 	canonical: SITE_PROTOCOL,
 	additionalMetaTags: [
@@ -14,14 +14,14 @@ export const DefaultSEOProps: DefaultSeoPropsType = {
 		{ name: 'identifier-URL', content: SITE_PROTOCOL },
 		{ name: 'shortlink', content: SITE_PROTOCOL },
 		{ name: 'summary', content: DESCRIPTION },
-		{ name: 'keywords', content: "uwu, nft, art, uwucrew, owo, uwu art, uwu nft, uwu labs, opensea, community" },
+		{ name: 'keywords', content: "uwu, nft, art, uwucrew, owo, uwu art, uwu nft, uwu labs, opensea, community, anime" },
 		{ name: 'robots', content: 'archive,follow,imageindex,index,odp,snippet,translate' },
 		{ name: 'googlebot', content: 'index,follow' },
 		{ name: 'reply-to', content: 'uwulabsteam@gmail.com' },
 		{ property: 'og:email', content: 'uwulabsteam@gmail.com' }
 	],
 	openGraph: {
-		title: 'uwucrew NFT Collection by uwu Labs',
+		title: 'uwucrew anime NFTs by uwu Labs',
 		url: SITE_PROTOCOL,
 		type: 'website',
 		locale: 'en_US',
